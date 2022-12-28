ac4d7c3d8266a12010060c780b9ecc821e267751
Harel Natan <harelnatan7@gmail.com>
2022-12-28 20:52:28 +0200

    Crud Add and Delete

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -1,26 +1,22 @@
 const { useNavigate, useParams, Link } = ReactRouterDOM
 
+import { NoteReview } from "./note-review.jsx"
+
 export function NoteList({ notes, onRemoveNote }) {
 
     const navigate = useNavigate()
 
-    function onSelectNote(id) {
-        console.log('id:', id)
-        navigate(`/note/edit/${id}`)
-    } 
+    // function onSelectNote(id) {
+    //     console.log('id:', id)
+    //     navigate(`/note/edit/${id}`)
+    // } 
     
-    return <ul className="notes-container grid clean-list">
+    return <ul className="notes-container grid clean-list" contenteditable="true">
         {notes.map(note => 
-            <li onClick={() => onSelectNote(note.id)} className="note" key={note.id} contentEditable="true">
-                {note.info.txt}
-                <div className="note-btn-container">
-                    <button className="fa-solid fa-pin"></button>
-                    <button className="fa-solid fa-color"></button>
-                    <button className="fa-solid fa-edit"></button>
-                    <button className="fa-solid fa-mail"></button>
-                    <button className="fa-solid fa-delete" onClick={() => onRemoveNote(event, note.id)}></button>
-                </div>
+            <li draggable className="note" key={note.id} >
+                <NoteReview note={note} onRemoveNote={onRemoveNote}/>
             </li>
         )}
     </ul>
+    
 }
