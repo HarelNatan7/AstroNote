4ceb873679821c8cb2c5fe28ad883920ba74523a
OrenKotSalomon <orensalomonkot@gmail.com>
2022-12-29 00:14:57 +0200

    Finished basic Mail crud on all needed components

--- apps/mail/cmps/mail-details.jsx
+++ apps/mail/cmps/mail-details.jsx
@@ -5,30 +5,66 @@ const { useParams, useNavigate, Link } = ReactRouterDOM
 import { mailServices } from "../services/mail.service.js"
 
 export function MailDetails() {
-    const [mail, setEmail] = useState(null)
+    const [mail, setMail] = useState(null)
+    const [mailtoUpdate, setMailToUpdate] = useState('')
     const { mailId } = useParams()
     const navigate = useNavigate()
 
     useEffect(() => {
         loadMail()
-    }, [mailId]);
+    }, []);
+
+    useEffect(() => {
+        updateMail()
+    }, [mailtoUpdate]);
 
     function loadMail() {
         mailServices.get(mailId).then((mail) => {
-            setEmail(mail)
+            setMail(mail)
         }).catch((err) => {
             console.log('got and error in details', err);
             navigate('/mail')
         })
     }
-    console.log(mail);
+
+    function updateMail() {
+        console.log(mailtoUpdate);
+
+        if (!mailtoUpdate) return
+        console.log(mailtoUpdate);
+        mailServices.put(mailtoUpdate).then((mailtoUpdate) => {
+
+            setMail(mailtoUpdate)
+            if (!mailtoUpdate.isTrash) return
+            navigate('/mail')
+        })
+    }
+
+    function onClickTrash() {
+        setMailToUpdate(() => {
+            if (!mail.isTrash) {
+                return { ...mail, isTrash: true }
+            } else return { ...mail, isTrash: false }
+        })
+    }
+
+    function onStarMail() {
+        setMailToUpdate(() => {
+            if (!mail.isStared) {
+                return { ...mail, isStared: true }
+            } else return { ...mail, isStared: false }
+        })
+
+    }
+
+
     if (!mail) return <h1>loading...</h1>
     return (
         <div className="mail-details">
             <div className="btn-nav-container">
                 <div className="btn-detail-container">
-                    <button className="fa-solid fa-delete"></button>
-                    <button className="fa-regular fa-star"></button>
+                    <button onClick={onClickTrash} className={`fa-solid fa-delete`}></button>
+                    <button onClick={onStarMail} className={`fa-regular fa-star ${mail.isStared ? 'stared' : ''}`}></button>
                     <button className="fa-regular fa-bookmark"></button>
                     <Link to="/mail" className="fa-solid fa-arrow-right-long arrow-back"></Link>
                 </div>
--- apps/mail/cmps/preview-expanded.jsx
+++ apps/mail/cmps/preview-expanded.jsx
@@ -2,21 +2,28 @@
 const { useState, useEffect, Fragment } = React
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
-export function PreviewExpanded({ mail }) {
+export function PreviewExpanded({ mail, updateMail }) {
     const [currExpandedMailId, setCurrExpandedMailId] = useState(null)
     const navigate = useNavigate()
+    useEffect(() => {
 
+    }, [mail]);
     function onClickExpandMail(mailId) {
         setCurrExpandedMailId(mailId)
         navigate(`/mail/${mailId}`)
     }
+    function deleteFromExpand() {
+        mail.isTrash = true
+        updateMail(mail)
+        console.log('hii');
+    }
 
     return (
-        <div className="main-expanded">
+        <div className={`main-expanded`}>
             <div className="expanded-subject-btn" >
                 <div colSpan='3' className="expanded-subject">{mail.subject}</div>
                 <div className="expanded-btn">
-                    <button className="fa-solid fa-delete"></button>
+                    <button onClick={deleteFromExpand} className="fa-solid fa-delete"></button>
                     <button className="fa-solid fa-expand" onClick={() => onClickExpandMail(mail.id)}></button>
                 </div>
             </div>
