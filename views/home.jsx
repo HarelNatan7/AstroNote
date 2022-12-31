5292bf33ede58e89707f569df3e8f239f8d62af8
Harel Natan <harelnatan7@gmail.com>
2022-12-31 18:26:12 +0200

    Logo Test

--- views/home.jsx
+++ views/home.jsx
@@ -2,7 +2,7 @@
 export function Home() {
 
     return <section className="home">
-        <img src='../assets/svg/Astronote-Cyan.svg' />
+        <img src='../../assets/svg/Astronote-Cyan.svg' />
         <h2>Everything you need</h2>
     </section>
 }
